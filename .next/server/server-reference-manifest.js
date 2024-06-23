5116d09f2200b14c9ee642803e5b385bce2e537b
Otávio Mendes <102169915+OtavioMendes12@users.noreply.github.com>
2024-06-22 21:08:31 -0300

    uodate

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Ev9tq2NgnuOu0F47JY+jIOwsMW06bA3Rr1I+Czy2gbE=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"V6zaRAq87cQUGAFIKVVcPE3hR1ZpelZmMJRv9euczP4=\"}"
